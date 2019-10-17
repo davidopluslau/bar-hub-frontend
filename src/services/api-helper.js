2d58003f2bdad1c448b8cb595dc3af7fc27df0cf
br305893 <ben.rosner.williamsburg@gmail.com>
2019-10-17 11:20:45 -0400

    change button padding to make text more centered

--- src/services/api-helper.js
+++ src/services/api-helper.js
@@ -1,27 +1,27 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:8080/drink'
+const baseUrl = 'http://localhost:8080/'
 
 const api = axios.create({
   baseURL: baseUrl
 })
 
 const createDrink = async (data) => {
-  const resp = await api.post('/drinks', { drink: data })
+  const resp = await api.post('/drink', { drink: data })
   return resp.data
 }
 
 const readAllDrinks = async () => {
-  const resp = await api.get('/drinks')
+  const resp = await api.get('/drink')
   return resp.data
 }
 
 const updateDrink = async (id, data) => {
-  const resp = await api.put(`/drinks/${id}`, {drink: data })
+  const resp = await api.put(`/drink/${id}`, {drink: data })
   return resp.data
 }
 
 const destroyDrink = async (id) => {
-  const resp = await api.delete(`/drinks/${id}`)
+  const resp = await api.delete(`/drink/${id}`)
   return resp
 }
 
